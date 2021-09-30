1a4320b253a1bbe1a304d578e4cf60869a9de2f1
razorhead4 <80808231+razorhead4@users.noreply.github.com>
2021-09-30 17:14:59 +0800

    ROBOFRIENDS
    ROBOFRIENDS

--- AdvanceLooping/script.js
+++ AdvanceLooping/script.js
@@ -1,3 +1,7 @@
+var _ = require('loadash');
+
+var array = [1,2,3,4];
+console.log('answer', _.without(array, 3));
 function moveFunction(){
     var i = 0;
 for(;i <= 10 && i >= -9;){
---
+++ NPM/script.js
@@ -0,0 +1,21 @@
+var _ = require('loadash');
+var array =[1,2,3,4,5,6,7]
+console.log('answer:',_.without(array, 4));
+function moveFunction(){
+    var i = 0;
+for(;i <= 10 && i >= -9;){
+    var position = prompt("enter a number");
+switch(position){
+            case "up":
+               console.log(i++);
+                break;
+            case "down":
+               console.log(i-=1);
+                break;
+             default:
+             console.log("invalid");
+             	break;
+}
+}
+}
+moveFunction(); 
